b05cdb42c8c27338c094fd99a669ff62fe3962d6
frappe <frappeccino0222@gmail.com>
2020-08-14 16:39:14 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,15 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def new
+    @items = Item.new
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :address
+  has_one :item_purchases
+  has_one_attached :image
 end
