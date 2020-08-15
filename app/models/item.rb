1b8cf562c7ecab9afb0b263b4f690a8110423f75
frappe <frappeccino0222@gmail.com>
2020-08-15 18:54:33 +0900

    アソシエーションの定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,13 +3,12 @@ class Item < ApplicationRecord
   has_one :address
   has_one :item_purchases
   has_one_attached :image
-  belongs_to_active_hash :category
+  belongs_to_active_hash :category,:status,:delivery_burden,:shipping_area,:estimated_shipping
 
   #空の投稿を保存できないようにする
   validates :name,:image,:price,:text,:category,:status,:delivery_burden,:shipping_area,:estimated_shipping, presence: true
 
   #ジャンルの選択が「--」の時は保存できないようにする
-  validates :category_id, numericality: { other_than: 1 } 
-end
+  validates :category_id,:status_id,:delivery_burden_id,:shipping_area_id,:estimated_shipping_id, numericality: { other_than: 1 } 
 
 end
