c084022aeeac2cfd238da2d5c029657133e5af28
frappe <frappeccino0222@gmail.com>
2020-08-18 15:30:42 +0900

    バリデーションの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,4 +17,8 @@ class Item < ApplicationRecord
   #ジャンルの選択が「--」の時は保存できないようにする
   validates :category_id,:status_id,:delivery_burden_id,:shipping_area_id,:estimated_shipping_id, numericality: { other_than: 1 } 
 
+  #価格が￥300〜¥9,999,999の間であること
+  validates :price, numericality: { only_integer: true,greater_than: 300, less_than: 9999999
+   }
+
 end
