de89afec0100f420880b713d807caf162dac6e6f
frappe <frappeccino0222@gmail.com>
2020-08-20 15:31:58 +0900

    非同期通信での販売手数料と販売利益の表示

---
+++ app/javascript/new.js
@@ -0,0 +1,22 @@
+  window.addEventListener('load', function(){
+    // 要素を取得
+    const input_price = document.getElementById("price")
+    console.log(input_price);
+
+    const total_price = document.getElementById("profit")
+    console.log(total_price);
+
+    const tax = document.getElementById("tax")
+    console.log(tax);
+
+
+
+   input_price.addEventListener('change', function(){
+    value = document.getElementById("price").value;
+      const date = (value * 0.9)
+      total_price.innerHTML = date
+      
+      const consumption = (value - date)
+      tax.innerHTML = consumption
+     })
+   })
