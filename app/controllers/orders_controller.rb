4935ee6362dd303d317b60e54e4727abe723a456
frappe <frappeccino0222@gmail.com>
2020-08-24 18:06:24 +0900

    アプリケーションで決済ができるように実装1

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,4 +4,31 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
   end
 
+  def create
+    @order = Order.new(price: order_params[:price])
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def order_params
+    params.permit(:price, :token)
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_XXXXXXXX"  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類(日本円)
+    )
+  end
+
 end
