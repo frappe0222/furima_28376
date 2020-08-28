4ea5cfb0ca2b394d3844befca0505e359ef64097
frappe <frappeccino0222@gmail.com>
2020-08-28 16:39:41 +0900

    ベーシック認証環境設定実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -11,6 +12,12 @@ class ApplicationController < ActionController::Base
     @post_image = PostImage.new
     @post_image.id = current_user.id
     @post_image.save  
- end
+  end
 
+ private
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+    username == ENV["BASIC_FURIMA_USER"] && password == ENV["BASIC_FURIMA_PASSWORD"]  # 環境変数を読み込む記述に変更
+    end
+  end
 end
